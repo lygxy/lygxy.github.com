a024c473209c97e00ca64b0d95e3bbf2fec0231d
lygxy <62172704+lygxy@users.noreply.github.com>
2020-12-14 20:42:55 +0800

    Add files via upload

---
+++ su_login/ex_code/mf_f_main.py
@@ -0,0 +1,26 @@
+
+
+from config import *
+# 右侧顺时针转一下90度
+# a1+R1=[
+#     [1,1,2],
+#     [1,1,2],
+#     [1,1,2]
+# ]
+# a1+R1=[
+#     [white,white,red],
+#     [white,white,red],
+#     [white,white,red]
+# ]
+
+# 右侧顺时针转360度
+# a1+R4=[
+#     [1,1,1],
+#     [1,1,1],
+#     [1,1,1]
+# ]
+# a1+R4=[
+#     [white,white,white],
+#     [white,white,white],
+#     [white,white,white]
+# ]
